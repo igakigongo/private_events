cb6c888dffd5283da3b83bc780a08c133c52ba0f
Edward Iga Kigongo <igakigongo@gmail.com>
2019-10-08 02:36:52 +0200

    correct failing tests

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'events/edit', type: :view do
+  let!(:user) do
+    User.create!(name: 'Edward')
+  end
+
+  let!(:evt) do
+    Event.create!(date: '2020-12-01',
+                  title: 'Some randon event',
+                  user: user)
+  end
+
   before(:each) do
-    @event = assign(:event, Event.create!)
+    @event = assign(:event, evt)
   end
 
   it 'renders the edit event form' do
--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'events/index', type: :view do
+  let!(:user) do
+    User.create!(name: 'Edward')
+  end
   before(:each) do
     assign(:events, [
-             Event.create!,
-             Event.create!
+             Event.create!(date: '2020-01-01', title: 'event 1', user: user),
+             Event.create!(date: '2020-02-02', title: 'event 2', user: user)
            ])
   end
 
